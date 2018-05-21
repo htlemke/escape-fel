863db001b7690f03690a175e61c795599f7c6a28
htlemke <henrik.lemke@psi.ch>
2018-05-21 22:54:18 +0200

    working on lazy functions for initialisation

--- storage/parse/swissfel.py
+++ storage/parse/swissfel.py
@@ -57,20 +57,35 @@ def parseScanEco_v01(file_name_json,search_paths=['./','./scan_data/','../scan_d
             else:
                 scan = Scan(parameter_names=s['parameter_names'], parameter_Ids=s['parameter_Ids'])
                 size_data = np.dtype(datasets[name][0].dtype).itemsize * datasets[name][0].size /1024**2
-                size_element = np.dtype(datasets[name][0].dtype).itemsize * np.prod(datasets[name][0].shape[1:]) /1024**2
-                chunk_size = 
-                dstores[name] = [da.from_array(datasets[name][0],chunks=datasets[name][0].chunks), \
-                                da.from_array(datasets[name][1], chunks=datasets[name][1].chunks),[len(datasets[name][0])]]
+                size_element = np.dtype(datasets[name][0].dtype).itemsize * np.prod(datasets[name][0].shape) /1024**2
+                chunk_size = dataset[name][0].chunks
+                if chunk_size[0] == 1:
+                    chunk_size[0] = memlimit_mD_MB//size_element
+                dstores[name] = {}
+                dstores[name]['scan'] = scan
+                dstores[name]['data'] = []
+                dstores[name]['data'].append(datasets[name][0])
+                dstores[name]['data_chunks'] = chunk_size
+                dstores[name]['eventIds'] = []
+                dstores[name]['eventIds'].append(datasets[name][1])
+                dstores[name]['stepLengths'] = []
+                dstores[name]['stepLemgths'].append(len(datasets[name][0]))
+                #dstores[name] = [da.from_array(datasets[name][0],chunks=datasets[name][0].chunks), \
+                #       da.from_array(datasets[name][1], chunks=datasets[name][1].chunks),[len(datasets[name][0])]]
                 names.add(name)
         for name in oldnames:
             if datasets[name][0].size ==0:
                 print("Found empty dataset in {} in cycle {}".format(name,stepNo))
             else:
-                dstores[name][0] = da.concatenate( [ dstores[name][0],
-                    da.from_array(datasets[name][0],chunks=datasets[name][0].chunks) ], axis=0)
-                dstores[name][1] = da.concatenate( [ dstores[name][1],
-                    da.from_array(datasets[name][1],chunks=datasets[name][1].chunks) ], axis=0)
-                dstores[name][2].append(len(datasets[name][0]))
+                dstores[name]['scan'].append()
+                dstores[name]['data'].append(datasets[name][0])
+                dstores[name]['eventIds'].append(datasets[name][1])
+                dstores[name]['stepLemgths'].append(len(datasets[name][0]))
+#                dstores[name][0] = da.concatenate( [ dstores[name][0],
+#                    da.from_array(datasets[name][0],chunks=datasets[name][0].chunks) ], axis=0)
+#                dstores[name][1] = da.concatenate( [ dstores[name][1],
+#                    da.from_array(datasets[name][1],chunks=datasets[name][1].chunks) ], axis=0)
+#                dstores[name][2].append(len(datasets[name][0]))
 
     escArrays = {}
     for name,(data,eventId,steplengths) in dstores.items():
