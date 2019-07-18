f72bb9ba0009ed75dd34cc03111fa03a1a752d77
Henrik Lemke <htlemke@gmail.com>
2019-07-18 21:56:32 +0200

    adding test data generator

---
+++ escape/storage/test_data.py
@@ -0,0 +1,17 @@
+from ..stream.testStream import TestData
+from .storage import Array
+import numpy as np
+from dask import array as da
+
+
+def get_test_data(N_pulses=1e4,as_array=True,as_da=True):
+    td = TestData()
+    d = { key:np.asarray(tl) for key,tl in zip(td.generateData(0).keys(), zip(*[list(td.generateData(n).values()) for n in range(int(N_pulses))])) }
+    if as_array:
+        pulse_id = d.pop('pulse_id')
+        if as_da:
+            d = {key:Array(data=da.from_array(td),eventIds=pulse_id,stepLengths=[len(td)]) for key,td in d.items()}
+        else:
+            d = {key:Array(data=td,eventIds=pulse_id,stepLengths=[len(td)]) for key,td in d.items()}
+    return d
+
